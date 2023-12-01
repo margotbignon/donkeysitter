e4d26f69eb53ffce7a565860c8ee274eaea6dba4
Margot BIGNON <margot.bignon78@gmail.com>
2023-12-01 15:22:53 +0100

    add suscribe master petsitter

--- Model/PetRepository.php
+++ Model/PetRepository.php
@@ -30,38 +30,58 @@ class PetRepository {
         return $pet;
     }
 
-    public function insertRow(Pet $pet): bool
+    public function insertRow(Pet $pet)
     {
-        $query = "INSERT INTO pets (name, image, description, race, gender, masterId, animalTypeId, sizeId) 
-                  VALUES (:name, :image, :description, :race, :gender, :masterId, :animalTypeId, :sizeId)";
-        
-        $statement = $this->pdo->prepare($query);
-        
-        $statement->bindValue(':name', $pet->getName(), PDO::PARAM_STR);
-        $statement->bindValue(':image', $pet->getImage(), PDO::PARAM_STR);
-        $statement->bindValue(':description', $pet->getDescription(), PDO::PARAM_STR);
-        $statement->bindValue(':race', $pet->getRace(), PDO::PARAM_STR);
-        $statement->bindValue(':gender', $pet->getGender(), PDO::PARAM_STR);
-        $statement->bindValue(':masterId', $pet->getMasterId(), PDO::PARAM_INT);
-        $statement->bindValue(':animalTypeId', $pet->getAnimalTypeId(), PDO::PARAM_INT);
-        $statement->bindValue(':sizeId', $pet->getSizeId(), PDO::PARAM_INT);
-        
-        return $statement->execute();
+        try {
+            if (empty($pet->getMasterId())) {
+                $errorMessages[] = "";
+            }
+            if (empty($pet->getMasterId()) && ((empty($pet->getName())) || (empty($pet->getYearBirth())) || (empty($pet->getYearBirth())) || (empty($pet->getDescription())) || (empty($pet->getRace())) ||(empty($pet->getGender())) || (empty($pet->getAnimalTypeId())) || (empty($pet->getSizeId())) )) {
+                $errorMessages[] = "";
+            } else {
+                if ((empty($pet->getName())) || (empty($pet->getYearBirth())) || (empty($pet->getYearBirth())) || (empty($pet->getDescription())) || (empty($pet->getRace())) ||(empty($pet->getGender())) || (empty($pet->getAnimalTypeId())) || (empty($pet->getSizeId())) ) {
+                    $errorMessages[] = "Veuillez renseigner tous les champs obligatoires.";
+                }
+
+            }
+
+            if (!empty($errorMessages)) {
+                throw new Exception (implode("<br><center>", $errorMessages));
+            }
+            
+            $query = "INSERT INTO pets (name, yearBirth, description, race, gender, master_id, animalType_id, size_id) 
+                    VALUES (:name, :yearBirth, :description, :race, :gender, :masterId, :animalTypeId, :sizeId)";
+            
+            $statement = $this->pdo->prepare($query);
+            
+            $statement->bindValue(':name', $pet->getName(), PDO::PARAM_STR);
+            $statement->bindValue(':yearBirth', $pet->getYearBirth(), PDO::PARAM_STR);
+            $statement->bindValue(':description', $pet->getDescription(), PDO::PARAM_STR);
+            $statement->bindValue(':race', $pet->getRace(), PDO::PARAM_STR);
+            $statement->bindValue(':gender', $pet->getGender(), PDO::PARAM_STR);
+            $statement->bindValue(':masterId', $pet->getMasterId(), PDO::PARAM_STR);
+            $statement->bindValue(':animalTypeId', $pet->getAnimalTypeId(), PDO::PARAM_INT);
+            $statement->bindValue(':sizeId', $pet->getSizeId(), PDO::PARAM_INT);
+            
+            return $statement->execute();
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
     }
 
 
     public function updateRow(Pet $pet): bool
     {
         $query = "UPDATE pets 
-                  SET name = :name, image = :image, description = :description, race = :race, gender = :gender, 
-                      masterId = :masterId, animalTypeId = :animalTypeId, sizeId = :sizeId 
+                  SET name = :name, yearBirth, description = :description, race = :race, gender = :gender, 
+                      master_id = :masterId, animalType_id = :animalTypeId, size_id = :sizeId 
                   WHERE idpet = :id";
         
         $statement = $this->pdo->prepare($query);
         
         $statement->bindValue(':id', $pet->getId(), PDO::PARAM_INT);
         $statement->bindValue(':name', $pet->getName(), PDO::PARAM_STR);
-        $statement->bindValue(':image', $pet->getImage(), PDO::PARAM_STR);
+        $statement->bindValue(':yearBirth', $pet->getYearBirth(), PDO::PARAM_STR);
         $statement->bindValue(':description', $pet->getDescription(), PDO::PARAM_STR);
         $statement->bindValue(':race', $pet->getRace(), PDO::PARAM_STR);
         $statement->bindValue(':gender', $pet->getGender(), PDO::PARAM_STR);
--- subscribe/subscribeMaster.php
+++ subscribe/subscribeMaster.php
@@ -2,11 +2,49 @@
 require_once "../Model/SizeRepository.php";
 require_once "../Model/AnimalTypesRepository.php";
 require_once "../Model/Database.php";
+require_once "../Model/Masters.php";
 require_once "../Model/config.php";
+require_once "../Model/MasterRepository.php";
+require_once "../Model/Pet.php";
+require_once "../Model/PetRepository.php";
 $sizeRepository = new SizeRepository();
 $sizes = $sizeRepository -> getRows();
 $animalTypesRepository = new AnimalTypesRepository();
 $animalTypes = $animalTypesRepository -> getRows();
+if (!empty($_POST)) {
+  $firstname = $_POST['firstname'];
+  $lastname = $_POST['lastname'];
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+  $confirmPassword = $_POST['confirmpassword'];
+  $street = $_POST['adresse'];
+  $postalCode = $_POST['postalCode'];
+  $city = $_POST['city'];
+  $phoneNb = $_POST['phoneNb'];
+  $birthDate = $_POST['birthDate'];
+  $birthDateDateTime = new DateTime($birthDate); 
+  $currentDate = new DateTime();
+  $ageDifference = $currentDate->diff($birthDateDateTime);
+  $image = $_FILES["profilPicture"]["tmp_name"];
+  $checkImage = $_FILES["profilPicture"]["error"];
+  $animalName = $_POST['animalName'];
+  $race = $_POST['race'];
+  $description = $_POST['description'];
+  $animalType = $_POST['animalType'];
+  $yearBirth = $_POST['yearBirth'];
+  $gender = $_POST['gender'];
+  $size = $_POST['size'];
+  $master = new Masters($firstname, $lastname, $phoneNb, $email, $password, $birthDate, $street, $postalCode, $city, $image);
+  $insertRowMasters = new MasterRepository();
+  if (!empty($_FILES['profilPicture']['name'])) {
+    $lastInsertId = $insertRowMasters->insertRowWithImage($master, $checkImage, $confirmPassword, $ageDifference);
+  } else {
+    $lastInsertId = $insertRowMasters->insertRow($master, $confirmPassword, $ageDifference);
+  }
+  $pet = new Pet($animalName, $description, $race, $gender, $yearBirth, $animalType, $size, $lastInsertId);
+  $insertRowMasters = new PetRepository();
+  $insertRowMasters->insertRow($pet);
+}
 ?>
 
 <!DOCTYPE html>
@@ -27,23 +65,33 @@ $animalTypes = $animalTypesRepository -> getRows();
   <body class="bg-warning" style="font-family:'Montserrat', sans-serif">
     <section class="container mt-5 bg-transparent suscribeform rounded w-50">
         <h1 class="text-center">Je veux confier mon animal <br>Inscription</h1>
-        <form method="post" class="d-flex  flex-column align-items-center">
+        <form method="post" class="d-flex  flex-column align-items-center" enctype="multipart/form-data">
           <div class="d-flex mt-2 w-75">
-            <input type="text" name="firstname" placeholder="Prénom" class="rounded border-0 p-2 w-50" >
+            <input type="text" name="firstname" placeholder="Prénom" class="rounded border-0 p-2 w-50">
             <input type="text" name="lastname" placeholder = "Nom" class="ms-2 rounded border-0 p-2 w-50">
           </div>
             <input type="email" name="email" placeholder = "Adresse E-mail" class="mt-3 w-75 rounded border-0 rounded border-0 p-2">
           <div class="d-flex w-75">
             <input type="password" name="password" placeholder = "Mot de passe" class="mt-3 rounded border-0 p-2 w-50">
-            <input type="password" name="confirmpassword" placeholder ="Confirmer votre mot de passe" class="mt-3 rounded border-0 p-2 ms-2 w-50">
+            <input type="password" name="confirmpassword" placeholder ="Confirmez votre mot de passe" class="mt-3 rounded border-0 p-2 ms-2 w-50">
           </div>
             <input type="text" name="adresse" placeholder = "Adresse" class="mt-3 rounded border-0 p-2 w-75">
+            <div class="d-flex mt-2 w-75">
+            <input type="text" name="postalCode" placeholder="Code Postal" class="rounded border-0 p-2 w-50" >
+            <input type="text" name="city" placeholder = "Ville" class="ms-2 rounded border-0 p-2 w-50">
+          </div>
+          
             <input type="phone" name="phoneNb" placeholder = "N° de téléphone" class="mt-3 rounded border-0 p-2 w-75">
+            <div class="text-left">
+              Votre date de naissance
+              <input type="date" name="birthDate" class="mt-3 rounded border-0 p-2 ">
+            </div>
             <div>Votre photo de profil
               <input type="file" name="profilPicture" placeholder = "Photo de Profil" class="mt-3">
             </div>
             <input type="text" name="animalName" placeholder = "Nom de votre animal" class="mt-3 rounded border-0 p-2 w-75">
             <input type="texte" name="race" placeholder = "Quelle est la race de votre animal ?" class="mt-3 rounded border-0 p-2 w-75">
+            <textarea name="description" placeholder = "Décrivez votre animal" class="mt-3 rounded border-0 p-2 w-75"></textarea>
             <select id="animalType" name = "animalType" class="mt-3 rounded border-0 p-2 w-75">
               <option value="" class="text-muted">Quel est le type de votre animal ?</option>
               <?php foreach ($animalTypes as $animalType) : ?>  
@@ -51,25 +99,14 @@ $animalTypes = $animalTypesRepository -> getRows();
               <?php endforeach ?>
             </select>
             <select id="yearBirth" name = "yearBirth" class="mt-3 rounded border-0 p-2 w-75">
-              <option value="">Quelle est l'année de naissance de votre animal ?</option>  
-              <option value="2023">2023</option>
-              <option value="2022">2022</option>
-              <option value="2021">2021</option>
-              <option value="2020">2020</option>
-              <option value="2019">2019</option>
-              <option value="2018">2018</option>
-              <option value="2017">2017</option>
-              <option value="2016">2016</option>
-              <option value="2015">2015</option>
-              <option value="2014">2014</option>
-              <option value="2013">2013</option>
-              <option value="2012">2012</option>
-              <option value="2011">2011</option>
-              <option value="2010">2010</option>
+              <option value="">Quelle est l'année de naissance de votre animal ?</option>
+              <?php for($i= 2023 ; $i >2009 ; $i--) : ?>
+                <option value="<?= $i ?>"><?=$i?></option>
+              <?php endfor ?>
             </select>
 
             <select id="gender" name = "gender" class="mt-3 rounded border-0 p-2 w-75">
-              <option value="" placeholder="Quel est le genre de votre animal ?">Quel est le genre de votre animal ?</option>
+              <option value="">Quel est le genre de votre animal ?</option>
               <option value="female">Femelle</option>
               <option value="male">Mâle</option>
             </select>
