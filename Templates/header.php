082ae40af77473599ea8e5e85aa8dce37d28a0f1
MelanieCoentreiras <melanie.coentreiras@gmail.com>
2023-12-01 14:54:54 +0100

    maj css and header

--- Templates/header.php
+++ Templates/header.php
@@ -9,12 +9,12 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="Templates/CSS/style.css">
     <!-- AOS animate  -->
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <!-- boxicons css -->
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
   </head>
-  <body class="bg-warning">
+  <body class="bg-warning body-image">
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <?php require './templates/nav.php'; ?>
