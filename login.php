108d9b461968bd99f46b058a488f7a91491c5867
MelanieCoentreiras <melanie.coentreiras@gmail.com>
2023-12-07 17:03:52 +0100

    require links modified

---
+++ login.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+include "Templates/header-var.php";
+require_once "Model/UsersRepository.php";
+require_once "Model/UsersLoginRepository.php";
+require_once "Model/Database.php";
+require_once "Model/Masters.php";
+require_once "Model/config.php";
+if (!empty($_POST)) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $login = new UsersLoginRepository();
+    $getLogin = $login->getUsertoLogin($email, $password);
+    if ($getLogin && password_verify($password, $getLogin['password'])) {
+
+        if (!empty($getLogin['idpetSitter'])) {
+            $SESSION['user']['id'] = $getLogin['idpetSitter'];
+        } else {
+            $SESSION['user']['id'] = $getLogin['idmaster'];
+        }
+            $SESSION['user']['role'] = $getLogin['role'];
+    } else if (!empty($password && !empty($email))) {
+        echo "<center>Les informations saisies n'ont pas permi de vous identifier. Veuillez vérifier vos informations</center>";
+    }
+    }
+
+?>
+<form method="post">
+    <h1>Connectez-vous</h1>
+    <input type="text" name="email" placeholder = "Votre email">
+    <input type="password" name="password" placeholder = "Votre mot de passe">
+    <input type="submit">
+<?php include "Templates/footer.php";?>
