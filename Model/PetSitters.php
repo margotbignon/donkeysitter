866f8e90f528f2e76d7220346ff5c92dbec6625e
Emilia RAFFALLI <e.raffalli@hotmail.fr>
2023-12-07 13:42:32 +0100

    animalTypesRepo / petsitters / petsitter Repo

--- Model/PetSitters.php
+++ Model/PetSitters.php
@@ -2,15 +2,34 @@
 require_once "Users.php";
 class PetSitters extends Users {
    
-
-    public function __construct(
-        int $id, string $firstName, string $lastName, string $phoneNb, string $email, string $passWord, string $birthDate, string $image, string $street, string $PostalCode, string $City,
-        private string $description, 
-        private string $petterSince, 
-        private int $residenceType_id,
-        
+        private string $description;
+        private string $petterSince;
+        private int $residenceType_id;
+        protected int $userId; 
+        protected int $animaltypeId;
+    
+        public function __construct(
+            int $petSitterId, 
+            string $firstName, 
+            string $lastName, 
+            string $phoneNb, 
+            string $birthDate, 
+            string $image, 
+            string $street, 
+            string $PostalCode, 
+            string $City,
+            string $description, 
+            string $petterSince, 
+            int $residenceType_id, 
+            int $animaltypeId, 
+            int $userId
         ) {
-            parent::__construct($id, $firstName, $lastName, $phoneNb, $email, $passWord, $birthDate, $image, $street, $PostalCode, $City);
+            parent::__construct($petSitterId, $firstName, $lastName, $phoneNb, $birthDate, $image, $street, $PostalCode, $City, $userId);
+            $this->description = $description;
+            $this->petterSince = $petterSince;
+            $this->residenceType_id = $residenceType_id;
+            $this->userId = $userId;
+            $this->animaltypeId=$animaltypeId;
         }
 
         public function getResidenceType_id() : int
@@ -47,6 +66,46 @@ class PetSitters extends Users {
         public function setDescription($description) : self
         {
                 $this->description = $description;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of userId
+         */ 
+        public function getUserId()
+        {
+                return $this->userId;
+        }
+
+        /**
+         * Set the value of userId
+         *
+         * @return  self
+         */ 
+        public function setUserId($userId)
+        {
+                $this->userId = $userId;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of animaltypeId
+         */ 
+        public function getAnimaltypeId()
+        {
+                return $this->animaltypeId;
+        }
+
+        /**
+         * Set the value of animaltypeId
+         *
+         * @return  self
+         */ 
+        public function setAnimaltypeId($animaltypeId)
+        {
+                $this->animaltypeId = $animaltypeId;
 
                 return $this;
         }
